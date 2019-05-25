0ace244a111ba6bbe66ca619f40aca9bb7f22475
chenliwen123 <1410762621@qq.com>
2019-05-25 13:50:29 +0800

    vue-cli主文件 将继续更新

---
+++ src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import $ from 'jquery'
+import axios from 'axios'
+import Swiper from 'swiper'
+import Vuex from 'vuex'
+import Store from './store/index.js'
+import 'swiper/dist/css/swiper.min.css'
+
+Vue.use(Vuex);
+Vue.prototype.$axios=axios;
+Vue.prototype.$Srote=Store;
+Vue.config.productionTip = false;
+
+axios.defaults.baseURL = "URL";
+axios.defaults.headers['Content-Type']='Application/x-www-form-urlencoded';
+axios.defaults.withCredentials=true;
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  Store,
+  components: { App },
+  template: '<App/>',
+  created () {},
+})
