6dfeb649e36c52450da6deec47ac27347df3b1f7
chenliwen123 <1410762621@qq.com>
2019-05-26 12:32:46 +0800

    更新了  getters 的相关状态

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    //{
+    //  path: '/',
+    //  name: 'Signin',
+    //  component: Signin
+    //},
+    //{
+    //  path: '/register',
+    //  name: 'register',
+    //  component: register
+    //},
+    //{
+    //  path: '/retrievepas',
+    //  name: 'retrievepas',
+    //  component: retrievepas
+    //},    {
+    //  path: '/zhmmcg',
+    //  name: 'zhmmcg',
+    //  component: zhmmcg
+    //}
+  ]
+})
