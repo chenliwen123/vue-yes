549fc5b307eab182a631eb9fb0ee8bb1d0df43ad
chenliwen123 <1410762621@qq.com>
2019-05-26 12:14:50 +0800

    默认的

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,29 @@ import Vuex from 'vuex'
 Vue.use(Vuex);
 const store=new Vuex.Store({
   state:{
-    name:"奥利给",
-    age:23
+    todos:[
+      {id:1,text:"...",done:false},
+      {id:2,text:"...",done:true},
+    ]
   },
   mutations:{
     showPeople:function (state,msg) {
       state.name=msg;
     }
   },
+  getters:{
+    doneTodos:state=>{
+      return state.todos.filter(todo=>todo.done);
+    },
+    doneTodosCount:(state,getters)=>{
+      return getters.doneTodos.length;
+    },
+    doneTodoid:(state)=>(id)=>{
+      return state.todos.find(todo=>todo.id===id);
+    }
+  }
 });
+console.log(store.getters.doneTodos);
+console.log(store.getters.doneTodosCount);
+console.log(store.getters.doneTodoid(store.state,2))
 export default  store
