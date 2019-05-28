43757def3d7e23be1794f067d402da5a4a0d5641
chenliwen123 <1410762621@qq.com>
2019-05-28 09:07:50 +0800

    mutations 是同步方法 actions   是异步方法 但是能修改state数据的只有mutations

--- src/store/index.js
+++ src/store/index.js
@@ -4,16 +4,29 @@ import Vuex from 'vuex'
 Vue.use(Vuex);
 const store=new Vuex.Store({
   state:{
+    name:0,
+    count:0,
     todos:[
       {id:1,text:"...",done:false},
       {id:2,text:"...",done:true},
     ]
-  },
+  },//存放数据的地方 只能在mutations中的函数里修改state的数据
   mutations:{
     showPeople:function (state,msg) {
       state.name=msg;
+    },
+    increment:function(){
+      store.state.count++;
     }
-  },
+  },//修改state的方法 调用时要store.commit('showPeople');   这里储存的方法都是同步不支持异步
+  actions:{
+    incrementAsync ({ commit }) {
+      setTimeout(() => {
+        commit('increment')
+        console.log('sssss');
+      }, 1000)
+    }
+  },//异步的方法  也可以调用mutations 中的方法  要store.commit(); 正常调用actions中的方法 store.dispatch();
   getters:{
     doneTodos:state=>{
       return state.todos.filter(todo=>todo.done);
@@ -24,9 +37,6 @@ const store=new Vuex.Store({
     doneTodoid:(state)=>(id)=>{
       return state.todos.find(todo=>todo.id===id);
     }
-  }
+  },
 });
-console.log(store.getters.doneTodos);
-console.log(store.getters.doneTodosCount);
-console.log(store.getters.doneTodoid(store.state,2))
 export default  store
