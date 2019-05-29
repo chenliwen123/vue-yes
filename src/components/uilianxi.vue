e12699485346838f446b5d74623b673bcc3ea9b9
chenliwen123 <1410762621@qq.com>
2019-05-29 08:58:00 +0800

    ui 组件的写法

--- src/components/uilianxi.vue
+++ src/components/uilianxi.vue
@@ -27,6 +27,87 @@
         <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
       </el-row>
 
+<el-row :gutter="20">
+  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+  <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+  <el-col :span="6"><div class="grid-content bg-purple-dark"></div></el-col>
+  <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+</el-row>
+      <!--
+      el-row 标签时固定一行 设置gutter 是el-col 每个列中间的距离 不包括首尾
+      el-col 是固定里面的列属性  el-row 100% 宽度是24
+      -->
+      <el-row :gutter="20">
+        <el-col :span="16"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="8"><div class="grid-content bg-purple-dark"></div></el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-dark"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-dark"></div></el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="16"><div class="grid-content bg-purple-dark"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+      </el-row>
+      <!--
+      el-row :gutter    设置el-col 中间距离
+      el-col  最大24 没有新的技术点
+      -->
+      <el-row :gutter="20">
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6" :offset="6"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="6" :offset="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6" :offset="6"><div class="grid-content bg-purple-light"></div></el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="12" :offset="6"><div class="grid-content bg-purple-dark"></div></el-col>
+      </el-row>
+      <!--
+      el-row   el-col
+      接触的新属性 :offset 偏移列
+      -->
+      <el-row :gutter="20" type="flex">
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+      <el-row :gutter="20" type="flex" justify="center" class="row-bg">
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+      <el-row :gutter="20" type="flex" justify="space-between">
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+      <el-row :gutter="20" type="flex" justify="space-around">
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+      <el-row :gutter="20" type="flex" justify="end">
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+      <!--
+      el-row flex 对齐方式
+      需在标签 el-row 中写入 type="flex" justify="写入对齐方式" 接收五个值    start(默认)、center、space-between、space-around、end
+      -->
+
+      <el-row :gutter="10">
+        <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple-light"></div></el-col>
+      </el-row>
+
     </div>
 </template>
 
