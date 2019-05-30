7598c693c1ccdb689cad4bedb35c0494d71d5207
chenliwen123 <1410762621@qq.com>
2019-05-30 08:37:03 +0800

    [+]

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <uibutton></uibutton>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import uilianxi from './components/uilianxi'
+import uibuju from './components/uibuju'
+import uibutton from './components/uibutton'
+export default {
+  name: 'App',
+  components: { HelloWorld,uilianxi,uibuju,uibutton},
+  template:{
+
+  },
+}
+</script>
+
+<style>
+  html,body{overflow-x:hidden;}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;}
+</style>
