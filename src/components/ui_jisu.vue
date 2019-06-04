cdf946576439ac57d9950f2252e55b4b92405fc5
chenliwen123 <1410762621@qq.com>
2019-06-04 08:52:19 +0800

    计数功能

---
+++ src/components/ui_jisu.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <el-row>
+          <el-input-number v-model="n1" :min="1" :max="10"></el-input-number>
+        </el-row>
+        <br>
+        <el-row>
+          <el-input-number v-model="n1" :min="1" :max="10" disabled></el-input-number>
+        </el-row>
+      <br>
+      <el-row>
+        <el-input-number v-model="n1" :step="2" :min="1" :max="10"></el-input-number>
+      </el-row>
+      <br>
+      <el-row>
+        <el-input-number v-model="n1" :step="2" step-strictly></el-input-number>
+      </el-row>
+      <br>
+      <el-row>
+        <el-input-number v-model="num" :step="0.1" step-strictly :precision="2" :max="10"></el-input-number>
+      </el-row>
+      <br>
+      <el-row>
+        <el-input-number v-model="n1" :max="10" :min="1" controls-position="right">
+
+        </el-input-number>
+      </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ui_jisu',
+  data(){
+    return{
+      n1:1,
+      num:1,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
