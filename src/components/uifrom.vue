210f10970a10753de11d813e1a5d6bbf373afca8
chenliwen123 <1410762621@qq.com>
2019-06-10 09:08:27 +0800

    element ui form  表单 差最后一个 整理总结

--- src/components/uifrom.vue
+++ src/components/uifrom.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <el-form :model="form" ref="form" label-width="80px">
+      <el-form :model="form" ref="form" label-width="80px" class="bzform">
         <el-form-item label="活动名称">
           <el-input v-model="form.name"></el-input>
         </el-form-item>
@@ -44,9 +44,53 @@
           <el-button>取消</el-button>
         </el-form-item>
       </el-form>
+      <br>
+      <hr>
+      <el-form :inline="true" :model="form1">
+        <el-form-item label="审批人">
+          <el-input placeholder="请填写审批人" v-model="form1.spr"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-select v-model="form1.hdqy">
+            <el-option value="区域一" label="区域一"></el-option>
+            <el-option value="区域二" label="区域二"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @submit.native.prevent>确定</el-button>
+        </el-form-item>
+      </el-form>
+      <br>
+      <hr>
+      <el-radio-group v-model="align" size="small">
+        <el-radio-button label="left" value="left">左对齐</el-radio-button>
+        <el-radio-button label="right" value="right">右对齐</el-radio-button>
+        <el-radio-button label="top" value="top">顶部对齐</el-radio-button>
+      </el-radio-group>
+      <el-form :model="form2" label-width="80px" :label-position="align">
+        <el-form-item label="名称">
+          <el-input v-model="form2.mingcheng" placeholder="请输入名称"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-select v-model="form2.quyu" placeholder="请选择活动区域">
+            <el-option label="区域一" value="区域一"></el-option>
+            <el-option label="区域二" value="区域二"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="活动形式">
+          <el-input placeholder="请填写活动形式" v-model="form2.xingshi"></el-input>
+        </el-form-item>
+      </el-form>
     </div>
 </template>
-
+<!--
+@submit.native.prevent  阻止默认行为
+form 表单上 :model= 的是一个对象  form表单内的v-model= 对象.属性
+:inline 让表单在一行显示 如果这一行放不下会换行
+el-form-item 有个 label 属性   这个属性就是行标题
+el-form label-width="80px"  这个80是  input 输入框 距离左侧的距离
+        :label-position 选择对齐方式
+-->
 <script>
 export default {
   name: 'uifrom',
@@ -61,6 +105,16 @@ export default {
         type:[],
         resource:'',
         desc:'',
+      },
+      form1:{
+        spr:'',
+        hdqy:'',
+      },
+      align:'left',
+      form2:{
+        mingcheng:"",
+        quyu:"",
+        xingshi:"",
       }
     }
   },
@@ -71,7 +125,7 @@ export default {
 </script>
 
 <style scoped>
-.el-form{max-width:640px;width:100%;}
+.bzform{max-width:640px;width:100%;}
   .el-select{width:100%;}
   .el-data-prcker{width:100%;}
   .el-input{width:100%;}
