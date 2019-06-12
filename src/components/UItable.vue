99ef3c362bbc44be75e9114093b72de2e23c9f7b
chenliwen123 <1410762621@qq.com>
2019-06-12 08:59:12 +0800

    element ui table 表格继续整理

--- src/components/UItable.vue
+++ src/components/UItable.vue
@@ -5,7 +5,7 @@
       stripe
       border
       height="250"
-      style="width:100px;"
+      style="width:100%;"
       >
         <el-table-column
         prop="date"
@@ -31,6 +31,79 @@
             label 列标题
             flex 固定某个列值
       -->
+      <br>
+      <br>
+      <el-table
+        ref="multipleTable"
+      :data="data"
+      style="width:100%;"
+      >
+        <el-table-column type="selection" width="55"></el-table-column>
+        <el-table-column label="日期" width="120"><template slot-scope="scope">{{scope.row.date}}</template></el-table-column>
+        <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+      </el-table>
+      <!--
+      添加一列 el-table-column 设置type="selection" 就是多选
+      -->
+      <br>
+      <br>
+        <el-table
+          :data="data"
+          style="width: 100%"
+        >
+          <el-table-column
+            prop="date"
+            label="日期"
+            sortable
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="姓名"
+            sortable
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="地址">
+          </el-table-column>
+        </el-table>
+      <!--
+      sortable 列排序
+      -->
+      <br>
+      <br>
+      <el-table
+      :data="data"
+      style="width:100%;">
+        <el-table-column label="日期" width="180">
+          <template slot-scope="scope">
+            <i class="el-icon-time"></i>
+            <span style="margin-left:10px">{{scope.row.date}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+        label="名称"
+        width="150"
+        >
+          <template slot-scope="scope">
+            <el-popover trigger="hover" placement="top">
+              <p>姓名:{{scope.row.name}}</p>
+              <p>地址:{{scope.row.address}}</p>
+              <div slot="reference">
+                <el-tag>{{scope.row.name}}</el-tag>
+              </div>
+            </el-popover>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="150">
+          <template slot-scope="scope">
+            <el-button size="mini" type="success">编辑</el-button>
+            <el-button size="mini" type="danger">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
 </template>
 
@@ -40,16 +113,18 @@ export default {
   data(){
     return{
       data:[
+        {date:"2016-05-01",name:"陈立文",address:"大连市甘井子区马兰街道马兰广场"},
         {date:"2016-05-02",name:"陈立文",address:"大连市甘井子区马兰街道马兰广场"},
-        {date:"2016-05-02",name:"陈立文",address:"大连市甘井子区马兰街道马兰广场"},
-        {date:"2016-05-02",name:"陈立文",address:"大连市甘井子区马兰街道马兰广场"},
-        {date:"2016-05-02",name:"陈立文",address:"大连市甘井子区马兰街道马兰广场"},
-        {date:"2016-05-02",name:"陈立文",address:"大连市甘井子区马兰街道马兰广场"},
+        {date:"2016-05-03",name:"陈立文",address:"大连市甘井子区马兰街道马兰广场"},
+        {date:"2016-05-04",name:"陈立文",address:"大连市甘井子区马兰街道马兰广场"},
+        {date:"2016-05-05",name:"陈立文",address:"大连市甘井子区马兰街道马兰广场"},
       ],
     }
   },
   methods:{
-
+    formatter(row, column) {
+      return row.address;
+    }
   }
 }
 </script>
