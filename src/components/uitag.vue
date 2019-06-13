da68f0143817863fd53d2167fe648befd184b696
chenliwen123 <1410762621@qq.com>
2019-06-13 15:45:14 +0800

    element ui tag 翻页pagination

--- src/components/uitag.vue
+++ src/components/uitag.vue
@@ -22,18 +22,76 @@
         <el-row>
           <el-col :span="10">
             <el-progress :percentage="50"></el-progress>
+            <el-progress :percentage="100" status="success"></el-progress>
+            <el-progress :percentage="50" status="exception"></el-progress>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col>
+            <el-progress :text-inside="true" :stroke-width="26" :percentage="70"></el-progress>
+            <el-progress :text-inside="true" :stroke-width="24" :percentage="100" status="success"></el-progress>
+            <el-progress :text-inside="true" :stroke-width="20" :percentage="50" status="exception"></el-progress>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col>
+            <!--<el-progress type="circle" :percentage="0"></el-progress>-->
+            <!--<el-progress type="circle" :percentage="25"></el-progress>-->
+            <!--<el-progress type="circle" :percentage="100" status="success"></el-progress>-->
+            <!--<el-progress type="circle" :percentage="50" status="exception"></el-progress>-->
+            <el-progress type="circle" :percentage="1"></el-progress>
+            <el-progress type="circle" :percentage="10"></el-progress>
+            <el-progress type="circle" :percentage="100" status="success"></el-progress>
+            <el-progress type="circle" :percentage="99" status="exception"></el-progress>
+            <el-progress type="circle" :percentage="percentage"></el-progress>
+          </el-col>
+        </el-row>
+        <!--
+  ref="name"
+  name 变成全局的对象
+  调用的时候this.$refs.name
+  closable 显示关闭按钮
+  需要关闭的话  需要用@close的方法
+  -->
+        <el-row>
+          <el-col>
+            <el-pagination layout="prev,pager,next" :total="50"></el-pagination>
+            <el-pagination layout="prev,pager,next" :total="1000"></el-pagination>
+            <el-pagination layout="prev,pager,next" :total="1000" background></el-pagination>
+            <el-pagination layout="prev,pager,next" :total="1000" small></el-pagination>
+
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col>
+            <el-pagination
+              :page-sizes="[100, 200, 300, 400]"
+              :page-size="100"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="400">
+            </el-pagination>
+            <el-pagination
+              :page-sizes="[100,200,300,400]"
+              :page-size="100"
+              layout="total,sizes,prev,pager,next,jumper"
+              :total="400"
+              small
+            >
+            </el-pagination>
+            <!--
+el-pagination
+    layout 设置显示内容 prev 上一页 next 下一页 pager 显示页码 total 显示总数 sizes 每页的个数 jumper 跳转到
+    :total 这是多少条记录
+    :page-size 100个一页
+    :page-sizes="[100,200,300,400]"   可以选择每页多少个
+    每十条记录 是一页
+    background 设置按钮背景
+    small 翻页变小
+-->
           </el-col>
         </el-row>
       </div>
     </div>
-  <!--
-    ref="name"
-    name 变成全局的对象
-    调用的时候this.$refs.name
-    closable 显示关闭按钮
-    需要关闭的话  需要用@close的方法
-
-    -->
 </template>
 
 <script>
@@ -44,6 +102,7 @@ export default {
       tag:['标签一','标签二','标签三'],
       addtags:'',
       addtagshow:false,
+      percentage:10,
     }
         },
   methods:{
@@ -62,6 +121,13 @@ export default {
         this.tag.push(this.addtags);
       }
       this.addtagshow=false;
+      this.addtags=null;
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
     }
   }
 }
