f040c88e63dcce664c3f2306e8b7d0814e96e2ea
chenliwen123 <1410762621@qq.com>
2019-07-17 20:36:39 +0800

    组件传值

---
+++ src/cz/f.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <z2 v-bind:massage="mag" v-on:listentochildevent="function123456"></z2>
+    </div>
+</template>
+
+<script>
+  import z2 from "./z2"
+    export default {
+      name: "f.vue",
+      components:{z2},
+      data(){
+          return{
+            mag:"hello,msg"
+          }
+      },
+      methods:{
+        function123456:function (data) {
+          console.log(data);
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/cz/z2.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h2>我是第二个子组件</h2>
+      <p>{{massage}}</p>
+      <el-button v-on:click="sendmsgtoparent" plain type="success">向父组件传值</el-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "z2",
+      props:["massage"],
+      data(){
+          return{
+          }
+      },
+      methods:{
+        sendmsgtoparent:function () {
+          this.$emit("listentochildevent","我是组价二 我是传过来的值");
+        }
+      }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
